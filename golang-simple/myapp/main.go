4d1558627fdcee381bf56d85f8f399a409eef1fa
諦念 <teinen.github@gmail.com>
2018-09-17 22:11:05 +0900

    Add sample http server file

---
+++ golang-simple/myapp/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/", HelloHandler).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, Go!")
+}
